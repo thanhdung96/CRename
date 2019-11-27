db057267a73fd1b380df256abb1531cd260168a8
thanhdung96 <14dh110224@st.huflit.edu.vn>
2019-11-27 15:47:35 +0700

    Added and finished numbering rule.
    Changes to be committed:
    	modified:   CRename.vcxproj
    	modified:   CRename.vcxproj.filters
    	new file:   src/CRuleNumber.cpp

---
+++ src/CRuleNumber.h
@@ -0,0 +1,38 @@
+#ifndef CRULE_NUMER_H
+#define CRULE_NUMER_H
+
+#include "CRule.h"
+#include <sstream>
+
+class CRuleNumber
+{
+public:
+	enum Direction{
+		Left = -1,
+		Right = 1
+	};
+
+	const unsigned INDEX_START = 1;
+
+public:
+	CRuleNumber();
+	~CRuleNumber();
+
+	string preview(const string& fileName);
+	bool apply(string& fileName);
+	string* toString();
+
+	void resetIndex();
+	void setPositionRelativeTo(const unsigned& position, const Direction& direction);
+
+private:
+	unsigned position;
+	unsigned storedPosition;
+	Direction positionRelativeTo;
+	unsigned index;
+
+private:
+	void makeValid(const unsigned& fileNameSize);
+};
+
+#endif
